c03f78ee697f167f41fbbf4da74eae72943e7622
Alexander Shabalin <gessman@outlook.com>
2021-08-09 00:27:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tgbot_calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
